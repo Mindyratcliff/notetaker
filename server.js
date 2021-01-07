32c0047f72ecfde1c7e9b74f26e4b6c90fd57f21
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-07 15:48:17 -0500

    Restructure of server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 //Imports
 const express = require("express");
+//Tell node to create the express server
 const app = express();
 
-​
+​//Initial Port
 const PORT = process.env.port || 8080;
 ​
-
-var notes = [];
 ​
 ​
 //Serves public folder
@@ -15,58 +14,13 @@ app.use(express.static(__dirname + "/public"));
 // Sets up the Express app to handle data parsing Allows assumption of JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Routes
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 ​
-/**
- * Get Calls
- */
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-​
-app.get("/notes", function (req, res) {
-    res.sendFile(__dirname + "/notes.html");
-    return res.json(notes);
-});
-​
-​
-app.get("/api/:funct", function (req, res) {
-    let funct = req.params.funct;
-​
-    switch (funct) {
-        case "notes":
-            res.send(notes);
-            break;
-        default:
-            res.sendStatus(404);
-            break;
-    }
-});
-​
-​
-/**
- * Post Calls
- */
-app.post("/api/notes", function (req, res) {
-​
-        var newNote = req.body;
-        notes.push(newNote);
-        res.json(newNote);
-        res.sendStatus(200);
-    
-​
-​
-});
-​
-/**
- * Delete Calls
- */
-app.delete("/api/clear", function (req, res) {
-    notes = [];
-​
-    res.sendStatus(200);
-});
-​
-​
+//Listener
 ​
 app.listen(PORT, function(){
 ​
