0bac38b9106cc77ea80af96b5fc3eb9727456e4f
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-09 13:37:19 -0500

    change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 
-let PORT = process.env.port || 8080;
+let PORT = process.env.port || 3000;
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended:true}));
