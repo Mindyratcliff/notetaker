7dcaa0f06388e35e4fcec862c69dec7660cd205d
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-11 17:04:10 -0500

    Moved writeFile to upper code block to correct issue.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = function(app) {
     
-  
+  var notes;
 
     app.get("/api/notes", function(req, res) {
       fs.readFile("db/db.json", (err, data) => {
@@ -12,26 +12,31 @@ module.exports = function(app) {
       
     });
   
-    function id (note) {
-      var noteId = 0;
-      for (let i = 0; i< notes.length; i++){
-          noteId = noteId + 1;        
-      }
-      return noteId + note;
-    }
+    
   
     app.post("/api/notes", function(req, res) {
-        var newNote = req.body;
+        
         fs.readFile("db/db.json", (err, data) => {
           if (err) throw err;
-          var notes = res.json(JSON.parse(data));
-          id(notes);
+          var notes = JSON.parse(data);
+          var noteId = 0;
+          for (let i = 0; i< notes.length; i++){
+          noteId = noteId + 1;        
+          }
+          var newNote = {
+            id: noteId,
+            title: req.body.title,
+            text: req.body.text
+          };
           notes.push(newNote);
+          
+          fs.writeFile("db/db.json", JSON.stringify(notes), "utf-8", (err, data) =>{
+            if (err) throw err;
+            console.log("A new note is added to the Note List.");
+          });
         })
-        fs.appendFile("db/db.json", JSON.stringify(notes), (err, data) =>{
-          if (err) throw err;
-          console.log("A new note is added to the Note List.");
-        });
+        
+        
 
         res.send(notes);
       
