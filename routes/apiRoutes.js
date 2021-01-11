a837930bf8c54c0575e7eece0bd1332840bcde16
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-11 15:51:32 -0500

    Adding unique ids

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,12 +12,20 @@ module.exports = function(app) {
       
     });
   
+    function id (note) {
+      var noteId = 0;
+      for (let i = 0; i< notes.length; i++){
+          noteId = noteId + 1;        
+      }
+      return noteId + note;
+    }
   
     app.post("/api/notes", function(req, res) {
         var newNote = req.body;
         fs.readFile("db/db.json", (err, data) => {
           if (err) throw err;
           var notes = res.json(JSON.parse(data));
+          id(notes);
           notes.push(newNote);
         })
         fs.appendFile("db/db.json", JSON.stringify(notes), (err, data) =>{
