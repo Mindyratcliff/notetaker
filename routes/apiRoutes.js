a38d6b9b339998360168deed9a15a841f9a3319f
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-12 15:08:57 -0500

    Delete function written to remove notes based upon id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,49 @@ const fs = require("fs");
 
 module.exports = function(app) {
     
-  var notes;
+  var notes = "";
 
     app.get("/api/notes", function(req, res) {
       fs.readFile("db/db.json", (err, data) => {
         if(err) throw err;
-        res.json(JSON.parse(data));
+        var notes = res.json(JSON.parse(data));
+        var noteId = 0;
+        for (let i = 0; i < notes.length; i++){
+        var id = {
+          id: noteId + 1,
+        }
+        notes[i].push(id)
+        
+        }
+
       })
       
+      
+
+      
+      
     });
+
+    app.delete("/api/notes/:id", function(req, res) {
+      fs.readFile("db/db.json", (err, data) => {
+        if(err) throw err;
+        var newNotes = [];        
+        var notes = JSON.parse(data);
+        for (var i = 0; i < notes.length; i++) {
+          if (notes[i].id != req.params.id) {
+            newNotes.push(notes[i]);
+          }
+        }
+      
+      fs.writeFile("db/db.json", JSON.stringify(newNotes), (err, data) => {
+        if (err) throw err;
+        console.log("A note has been deleted.");
+          
+      })
+      res.send(newNotes);
+    })
   
-    
+  })
   
     app.post("/api/notes", function(req, res) {
         
@@ -42,18 +74,5 @@ module.exports = function(app) {
       
     });
   
-  
-    app.DELETE("/api/notes/", function(req, res) {
-        
-        fs.writeFile("db/db.json", JSON.stringify(notes), "utf-8", (err, data) =>{
-          if (err) throw err;
-          
-
-          notes = "";
-
-          
-        });
-        return res.send(notes);
-        });
     };
           
